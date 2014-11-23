0a7adf896ac9262bc899296ed3ff94fca98c970c
Olivier Lange <olange@petit-atelier.ch>
2014-11-23 18:04:57 +0100

    Added method to write config file

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf_8 -*-
 
 import ConfigParser
+import codecs
 
 def read( config_filename='hamster-migrate.cfg'):
   cfg = ConfigParser.RawConfigParser()
   cfg.read( config_filename)
   return cfg
+
+def write( cfg, config_filename='hamster-migrate.cfg'):
+  with codecs.open( config_filename, encoding='utf-8', mode='wb') as cfg_file:
+    cfg.write( cfg_file)
