4b93668c5e0ce4b4a62688dcdffeddddc2d17890
Olivier Lange <olange@petit-atelier.ch>
2014-11-23 19:53:56 +0100

    Added logging to the config reader/writer

--- config.py
+++ config.py
@@ -3,12 +3,17 @@
 
 import ConfigParser
 import codecs
+import logging
 
-def read( config_filename='hamster-migrate.cfg'):
+logger = logging.getLogger( __name__)
+
+def read( config_filename="hamster-migrate.cfg"):
+  logger.info( u"Reading configuration from {0}".format( config_filename))
   cfg = ConfigParser.RawConfigParser()
   cfg.read( config_filename)
   return cfg
 
-def write( cfg, config_filename='hamster-migrate.cfg'):
-  with codecs.open( config_filename, encoding='utf-8', mode='wb') as cfg_file:
+def write( cfg, config_filename="hamster-migrate.cfg"):
+  logger.info( u"Writing configuration to {0}".format( config_filename))
+  with codecs.open( config_filename, encoding="utf-8", mode="wb") as cfg_file:
     cfg.write( cfg_file)
