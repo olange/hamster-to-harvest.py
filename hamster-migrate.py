c215bfe55d7f3df4afdea32776c24e7826228ff8
Olivier Lange <olange@petit-atelier.ch>
2014-11-24 14:41:01 +0100

    Renamed config.py to options.py

--- hamster-migrate.py
+++ hamster-migrate.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import sys, codecs, logging, datetime
 import hamster.db
 import api.harvest
-import cli, config
+import cli, options
 
 logging.basicConfig( level=logging.INFO)
 logger = logging.getLogger( __name__)
@@ -36,35 +36,35 @@ def dump_projects_and_tasks( harvest_client, stream=sys.stdout):
     for assignment in project.task_assignments:
       print( u"\t{0}".format( str( assignment)), file=stream)
 
-def seed_tasks( config, harvest_client):
+def seed_tasks( opts, harvest_client):
   """Seed the configuration file with the list of tasks IDs and names from Harvest."""
-  config.remove_section( "Harvest Tasks")
-  config.add_section( "Harvest Tasks")
+  opts.remove_section( "Harvest Tasks")
+  opts.add_section( "Harvest Tasks")
   for task in harvest_client.tasks():
     task = vars( task)
-    config.set( "Harvest Tasks", task["name"], str( task["id"]))
+    opts.set( "Harvest Tasks", task["name"], str( task["id"]))
     logger.info( u"Harvest task {name} ({id})".format( **task))
 
-def seed_projects( config, harvest_client):
+def seed_projects( opts, harvest_client):
   """Seed the configuration file with the list of project IDs and names from Harvest."""
-  config.remove_section( "Harvest Projects")
-  config.add_section( "Harvest Projects")
+  opts.remove_section( "Harvest Projects")
+  opts.add_section( "Harvest Projects")
   for project in harvest_client.projects():
     project = vars( project)
-    config.set( "Harvest Projects", project["name"], str( project["id"]))
+    opts.set( "Harvest Projects", project["name"], str( project["id"]))
     logger.info( u"Harvest project {name} ({id})".format( **project))
 
-def seed( args, cfg):
-  harvest_url = cfg.get( "Harvest", "url")
-  harvest_email = cfg.get( "Harvest", "email")
-  harvest_pwd = cfg.get( "Harvest", "password")
+def seed( args, opts):
+  harvest_url = opts.get( "Harvest", "url")
+  harvest_email = opts.get( "Harvest", "email")
+  harvest_pwd = opts.get( "Harvest", "password")
   logger.info( u"Accessing Harvest timesheet at {0}".format( harvest_url))
   harvest_client = api.harvest.Harvest( harvest_url, harvest_email, harvest_pwd)
   with codecs.open( "harvest-ids.txt", encoding='utf-8', mode='wb') as harvest_ids_file:
     dump_projects_and_tasks( harvest_client, harvest_ids_file)
 
-def migrate( args, cfg):
-  hamster_db_dir = cfg.get( "Hamster", "database-dir")
+def migrate( args, opts):
+  hamster_db_dir = opts.get( "Hamster", "database-dir")
   logger.info( u"Reading Hamster time entries from {0}".format( hamster_db_dir))
   storage = hamster.db.Storage( database_dir=hamster_db_dir)
   list_facts( storage, args.start, args.end, args.search)
@@ -72,13 +72,13 @@ def migrate( args, cfg):
 def main( argv):
   """Parse the command-line args and call the function matching
   the subcommand that was selected."""
-  cfg = config.read( "hamster-migrate.cfg")
+  opts = options.read( "hamster-migrate.cfg")
 
   args = cli.parser.parse_args( argv)
   logger.info( args)
 
   subcommands = { "seed": seed, "migrate": migrate }
-  subcommands[ args.subcommand]( args, cfg)
+  subcommands[ args.subcommand]( args, opts)
 
 if __name__ == '__main__':
   main( sys.argv[1:])
