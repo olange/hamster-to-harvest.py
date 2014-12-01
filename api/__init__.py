1ce2a94229ad577f7cf24dbb00f765917341babe
Olivier Lange <olange@petit-atelier.ch>
2014-12-01 23:19:30 +0100

    Added a copy of Python Harvest Time Tracking API
    `pip install harvest-time-tracking-api-client` not working sadly (see
    https://github.com/lionheart/python-harvest/issues/2)

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 
-# The 'harvest.py' is a copy of https://github.com/lann/Harvest/blob/master/harvest.py
-# because it was not available for installation with 'pip install'.
-# So I reproduced its contents; credits go to the authors of the library.
+# The Harvest Time Tracking API offers read/write access to the Time Tracking entities.
+# The Python Harvest API Wrapper offers read-only access to all entities of Harvest.
+# So I needed both.
 
-__title__ = "harvest-api"
-__version__ = "1.0"
-__license__ = "GNU AFFERO GENERAL PUBLIC LICENSE 3.0"
+# Harvest Time Tracking API
+# https://github.com/lionheart/python-harvest
+#
+# The file 'harvest_time_tracking.py' of this directory is a copy of:
+#
+#   https://github.com/lionheart/python-harvest/blob/master/harvest/harvest.py
+#
+# with a slight modification to further fix an issue. I embedded it because it
+# can't currently be installed with 'pip install' and added to the requirements.
+# (see https://github.com/lionheart/python-harvest/issues/1 and 2).
+# Credits go to the authors.
+
+from . import harvest_time_tracking
+
+# Python Harvest API Wrapper
+# https://github.com/lann/Harvest
+#
+# The file 'harvest.py' of this directory is a copy of:
+#
+#   https://github.com/lann/Harvest/blob/master/harvest.py
+#
+# which I embedded, because it was not available for installation
+# with 'pip install'. Credits go to the authors.
 
 from . import harvest
